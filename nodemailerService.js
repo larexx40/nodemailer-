bc6d32c788f412d42c776dbd0491664ca55ba21a
larexx40 <larexx40@gmail.com>
2022-05-07 11:34:23 +0100

    send mail with handlebar(HTML template)

--- nodemailerService.js
+++ nodemailerService.js
@@ -2,7 +2,11 @@ require('dotenv').config();
 const hbs = require('nodemailer-express-handlebars')
 const  nodemailer = require('nodemailer');
 const path = require('path')
-
+/**
+ * NB. you must allow receive message from 
+ * less secure app in your gmail
+ * (both sender and receeiver gmail)
+*/
 //initialize nodemailer transporter
 let transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -12,9 +16,25 @@ let transporter = nodemailer.createTransport({
     }
 })
 
+
+const handlebarOption = {
+    viewEngine: {
+        partialsDir: path.resolve('./views/'),
+        defaultLayout: false,
+    }, 
+    viewPath: path.resolve('./views/'),
+}
+
+transporter.use('compile', hbs(handlebarOption))
+
 let mailOptions ={
-    from: 'lanretunji47@gmail.com',
+    from: '"Lanre" <lanretunji47@gmail.com>',
     to: 'o.o.rokeeb@gmail.com',
+    template: 'email',//email.handlebars
+    context:{
+        name: 'Lanre', //name on handlebar
+        company: 'ZinoBank' //company 
+    },
     subject: 'Node.js Test Mail',
     text: 'welcome to nodemailer module, test how to send mail to user via nodemailer.'
 }
@@ -26,14 +46,3 @@ transporter.sendMail(mailOptions, (err, info)=>{
         console.log('message sent sucessfully');
     }
 })
-
-/*
-handlebars template
-const handlebarOption = {
-    viewEngine: {
-        partialsDir: path.resolve('./views/'),
-        defaultLayout: false,
-    }, 
-    viewPath: path.resolve('./views/'),
-}
-*/
